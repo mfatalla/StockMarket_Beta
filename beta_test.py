327ad69891865eec640ea308b7491b752edcd3c1
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-09 01:29:20 +0800

    Add files via upload

--- beta_test.py
+++ beta_test.py
@@ -20,6 +20,7 @@ import numpy as np
 from yahooquery import Ticker
 import datetime as dt
 import yfinance as yf
+import profile2
 
 
 
@@ -72,6 +73,7 @@ asset = st.sidebar.selectbox(
     index=3,
     format_func=label,
 )
+
 ticker = yf.Ticker(asset)
 info = ticker.info
 url = 'https://stockanalysis.com/stocks/' + asset
@@ -628,83 +630,7 @@ elif menubar == 'Technical Indicators':
 
 
 elif menubar == 'Company Profile':
-    profile = st.beta_container()
-    with profile:
-        ticker = yf.Ticker(asset)
-        info = ticker.info
-
-        st.title('Company Profile')
-        st.subheader(info['longName'])
-        st.markdown('** Sector **: ' + info['sector'])
-        st.markdown('** Industry **: ' + info['industry'])
-        st.markdown('** Phone **: ' + info['phone'])
-        st.markdown(
-            '** Address **: ' + info['address1'] + ', ' + info['city'] + ', ' + info['zip'] + ', ' + info['country'])
-        st.markdown('** Website **: ' + info['website'])
-        st.markdown('** Business Summary **')
-        st.info(info['longBusinessSummary'])
-
-        fundInfo = {
-            'Enterprise Value (USD)': info['enterpriseValue'],
-            'Enterprise To Revenue Ratio': info['enterpriseToRevenue'],
-            'Enterprise To Ebitda Ratio': info['enterpriseToEbitda'],
-            'Net Income (USD)': info['netIncomeToCommon'],
-            'Profit Margin Ratio': info['profitMargins'],
-            'Forward PE Ratio': info['forwardPE'],
-            'PEG Ratio': info['pegRatio'],
-            'Price to Book Ratio': info['priceToBook'],
-            'Forward EPS (USD)': info['forwardEps'],
-            'Beta ': info['beta'],
-            'Book Value (USD)': info['bookValue'],
-            'Dividend Rate (%)': info['dividendRate'],
-            'Dividend Yield (%)': info['dividendYield'],
-            'Five year Avg Dividend Yield (%)': info['fiveYearAvgDividendYield'],
-            'Payout Ratio': info['payoutRatio']
-        }
-
-        fundDF = pd.DataFrame.from_dict(fundInfo, orient='index')
-        fundDF = fundDF.rename(columns={0: 'Value'})
-        st.subheader('Fundamental Info')
-        st.table(fundDF)
-
-        st.subheader('General Stock Info')
-        st.markdown('** Market **: ' + info['market'])
-        st.markdown('** Exchange **: ' + info['exchange'])
-        st.markdown('** Quote Type **: ' + info['quoteType'])
-
-        start = dt.datetime.today() - dt.timedelta(2 * 365)
-        end = dt.datetime.today()
-        df = yf.download(asset, start, end)
-        df = df.reset_index()
-        fig_prof = go.Figure(
-            data=go.Scatter(x=df['Date'], y=df['Adj Close'])
-        )
-        fig_prof.update_layout(
-            title={
-                'text': "Stock Prices Over Past Two Years",
-                'y': 0.9,
-                'x': 0.5,
-                'xanchor': 'center',
-                'yanchor': 'top'})
-        st.plotly_chart(fig_prof, use_container_width=True)
-
-        marketInfo = {
-            "Volume": info['volume'],
-            "Average Volume": info['averageVolume'],
-            "Market Cap": info["marketCap"],
-            "Float Shares": info['floatShares'],
-            "Regular Market Price (USD)": info['regularMarketPrice'],
-            'Bid Size': info['bidSize'],
-            'Ask Size': info['askSize'],
-            "Share Short": info['sharesShort'],
-            'Short Ratio': info['shortRatio'],
-            'Share Outstanding': info['sharesOutstanding']
-
-        }
-
-        marketDF = pd.DataFrame(data=marketInfo, index=[0])
-        st.table(marketDF)
-        st.write(info)
+    profile2.Profile(asset)
 
 elif menubar == 'About':
     st.write("")
