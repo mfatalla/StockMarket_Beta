a8633f458963370ee426d6b855d2bfa09a191c3c
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-09 02:04:26 +0800

    Add files via upload

--- about.py
+++ about.py
@@ -82,8 +82,8 @@ def About():
     st.write("")
     project_depend = st.beta_expander("Project Dependencies", expanded=False)
     with project_depend:
-        first_col = ['streamlit', 'pandas', 'request', 'bs4', 'beautifulsoup4', 'lmxl', 'yfinance', 'plotly', 'numpy']
-        second_col = ['0.84.0', '1.2.4', '2.25.1', '0.0.1', '4.9.3', '4.6.3', '0.1.59', '4.14.3', '1.20.2']
+        first_col = ['streamlit', 'pandas', 'request', 'bs4', 'beautifulsoup4', 'lmxl', 'yfinance', 'plotly', 'numpy', 'matploblib', 'yahooquery','plotly','statsmodel','pmdarima', 'scikit-learn']
+        second_col = ['0.84.0', '1.2.4', '2.25.1', '0.0.1', '4.9.3', '4.6.3', '0.1.59', '4.14.3', '1.20.2', '3.4.2', '2.2.15', '4.14.3', '0.12.2', '1.8.2', '0.24.2']
         requirements = pd.DataFrame(
             {"Dependencies": list(first_col), "Version": list(second_col)})
         requirements.index = [""] * len(requirements)
