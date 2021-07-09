296533f7c0aad704ea019a7fb10d67ceedfe5d97
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-10 03:59:11 +0800

    Add files via upload

--- overview.py
+++ overview.py
@@ -160,7 +160,42 @@ def Overview(asset):
     candle(asset)
     left, right = st.beta_columns([1, 1])
     with left:
-        st.write("WAP")
+        summarytable = st.beta_container()
+        with summarytable:
+            urlfortable = 'https://stockanalysis.com/stocks/' + asset
+            page = requests.get(urlfortable)
+            doc = lh.fromstring(page.content)
+            tr_elements = doc.xpath('//tr')
+            i = 0
+            i2 = 0
+            tablecount = 0
+            mylist1 = []
+            mylist2 = []
+            mylist3 = []
+            mylist4 = []
+            for tablecount in range(9):
+                for t in tr_elements[tablecount]:
+                    i += 1
+                    if (i % 2) == 0:
+                        value1 = t.text_content()
+                        mylist1.append(str(value1))
+                    else:
+                        name1 = t.text_content()
+                        mylist2.append(str(name1))
+            for tablecount2 in range(9, 18):
+                for t2 in tr_elements[tablecount2]:
+                    i2 += 1
+                    if (i2 % 2) == 0:
+                        value2 = t2.text_content()
+                        mylist3.append(str(value2))
+                    else:
+                        name2 = t2.text_content()
+                        mylist4.append(str(name2))
+            final_table = pd.DataFrame(
+                {"": list(mylist2), "Value": list(mylist1), " ": list(mylist4), "Value ": list(mylist3)})
+            final_table.index = [""] * len(final_table)
+            st.subheader("Summary")
+            st.table(final_table)
 
     with right:
         st.subheader("About")
