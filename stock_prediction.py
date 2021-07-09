a600ec52d838565010a594c30898736ab8425a88
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-09 15:40:12 +0800

    Add files via upload

--- stock_prediction.py
+++ stock_prediction.py
@@ -168,7 +168,7 @@ def stock_predict(tickerinput):
         plt.plot(std_dev, color="black", label="Standard Deviation")
         plt.plot(moving_avg, color="red", label="Mean")
         plt.legend()
-        st.pyplot(df_log)
+        st.pyplot(rcParams['figure.figsize'])
 
 
     with part4:
