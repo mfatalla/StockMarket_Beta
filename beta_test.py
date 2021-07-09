c1b119688c4d882fee37e1787e41a9f3cd1358bf
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-10 02:59:29 +0800

    Add files via upload

--- beta_test.py
+++ beta_test.py
@@ -57,7 +57,7 @@ with sidebar_components:
     st.image('data//logo1.png')
     st.write("")
     asset = st.sidebar.selectbox(
-        "Click below to select a new asset",
+        "Click below to select new asset",
         components.index.sort_values(),
         index=3,
         format_func=label,
@@ -70,19 +70,22 @@ with sidebar_components:
     soup = BeautifulSoup(response.text, 'lxml')
     name = soup.find('h1', {'class': 'sa-h1'}).text
     price = soup.find('span', {'id': 'cpr'}).text
-    currency = soup.find('span', {'class': 'scurr'}).text
+    currency = soup.find('span', {'id': 'cpr'}).find_next('span').text
     change = soup.find('span', {'id': 'spd'}).text
+    rate = soup.find('span', {'id': 'spd'}).find_next('span').text
     meta = soup.find('div', {'id': 'sti'}).find('span').text
     try:
-        price2 = soup.find('span', {'id': 'extpr'}).text
+        after = soup.find('div', {'id': 'ext'}).find_next('span').text
         after2 = soup.find('span', {'id': 'extc'}).text
         aftert = soup.find('span', {'id': 'extcp'}).text
         aftertime = soup.find('span', {'id': 'exttime'}).text
+        CR = change + " (" + rate + ")"
+        CT = after2 + " (" + aftert + ")"
         sub = change
         sub2 = after2
         aye = ": After-hours"
     except AttributeError as attriErr:
-        price2 = "NO DATA"
+        after = "NO DATA"
         after2 = "NO DATA"
         aftert = "NO DATA"
         aftertime = "NO DATA"
@@ -105,29 +108,26 @@ with sidebar_components:
             unsafe_allow_html=True)
         if sub != "NO DATA" and sub2 != "NO DATA":
             if float(sub) > 0:
-                rate = soup.find('span', {'id': 'spcol', 'class': 'sp_green'}).text
+                aye2 = "+"
                 st.markdown(
-                    f"<p style='vertical-align:bottom;font-weight: bold; color: #00AC4A;font-size: 13px;'>{rate}</p>",
+                    f"<p style='vertical-align:bottom;font-weight: bold; color: #00AC4A;font-size: 13px;'>{aye2 + CR}</p>",
                     unsafe_allow_html=True)
             else:
-                rate = soup.find('span', {'id': 'spcol', 'class': 'sp_red'}).text
                 st.markdown(
-                    f"<p style='vertical-align:bottom;font-weight: bold; color: #D10000;font-size: 13px;'>{rate}</p>",
+                    f"<p style='vertical-align:bottom;font-weight: bold; color: #D10000;font-size: 13px;'>{CR}</p>",
                     unsafe_allow_html=True)
             st.markdown(
                 f"<p style='vertical-align:bottom;font-weight: italic; color: #FFFFFF;font-size: 10px;'>{meta}</p>",
                 unsafe_allow_html=True)
             if float(sub2) > 0:
-                st.markdown(price2 + " " + currency)
-                rate2 = soup.find('span', {'id': 'extcol', 'class': 'sp_green'}).text
+                st.markdown(after + " " + currency)
                 st.markdown(
-                    f"<p style='vertical-align:bottom;font-weight: bold; color: #00AC4A;font-size: 13px;'>{rate2}</p>",
+                    f"<p style='vertical-align:bottom;font-weight: bold; color: #00AC4A;font-size: 13px;'>{CT + aye}</p>",
                     unsafe_allow_html=True)
             else:
-                st.markdown(price2 + " " + currency)
-                rate2 = soup.find('span', {'id': 'extcol', 'class': 'sp_red'}).text
+                st.markdown(after + " " + currency)
                 st.markdown(
-                    f"<p style='vertical-align:bottom;font-weight: bold; color: #D10000;;font-size: 13px;'>{rate2}</p>",
+                    f"<p style='vertical-align:bottom;font-weight: bold; color: #D10000;;font-size: 13px;'>{CT + aye}</p>",
                     unsafe_allow_html=True)
             st.markdown(
                 f"<p style='vertical-align:bottom;font-weight: italic; color: #FFFFFF;font-size: 10px;'>{aftertime}</p>",
