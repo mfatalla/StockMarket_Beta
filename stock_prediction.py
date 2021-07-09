1565ea9213924ff6e2d1bfed913f44b973336b29
mfatalla <80556524+mfatalla@users.noreply.github.com>
2021-07-09 15:51:20 +0800

    Add files via upload

--- stock_prediction.py
+++ stock_prediction.py
@@ -108,132 +108,123 @@ def stock_predict(tickerinput):
         st.markdown(tmp_download_link, unsafe_allow_html=True)
 
 
-    part1, part2 = st.beta_columns(2)
-    with part1:
-        line_fig = plt.figure(figsize=(10, 6))
-        plt.grid(True)
-        plt.xlabel('Dates')
-        plt.ylabel('Close Prices')
-        plt.plot(datatest['Close'])
-        plt.title((info['longName']) + ' closing price')
+    line_fig = plt.figure(figsize=(10, 6))
+    plt.grid(True)
+    plt.xlabel('Dates')
+    plt.ylabel('Close Prices')
+    plt.plot(datatest['Close'])
+    plt.title((info['longName']) + ' closing price')
 
-        st.subheader("Figure1")
-        st.pyplot(line_fig)
+    st.subheader("Figure1")
+    st.pyplot(line_fig)
 
-    with part2:
-        df_close = datatest['Close']
-        df_close.plot(style='k.')
-        plt.title('Scatter plot of closing price')
-        scatter_fig = line_fig
+    df_close = datatest['Close']
+    df_close.plot(style='k.')
+    plt.title('Scatter plot of closing price')
+    scatter_fig = line_fig
 
-        st.subheader("Figure 2")
-        st.pyplot(scatter_fig)
+    st.subheader("Figure 2")
+    st.pyplot(scatter_fig)
 
-    part3, part4 = st.beta_columns(2)
-    with part3:
-        def test_stationarity(timeseries):
-            # Determing rolling statistics
-            rolmean = timeseries.rolling(12).mean()
-            rolstd = timeseries.rolling(12).std()
-            # Plot rolling statistics:
-            plt.plot(timeseries, color='blue', label='Original')
-            plt.plot(rolmean, color='red', label='Rolling Mean')
-            plt.plot(rolstd, color='black', label='Rolling Std')
-            plt.legend(loc='best')
-            plt.title('Rolling Mean and Standard Deviation')
-            plt.show(block=False)
-
-            adft = adfuller(timeseries, autolag='AIC')
-            # output for dft will give us without defining what the values are.
-            # hence we manually write what values does it explains using a for loop
-            output = pd.Series(adft[0:4],
-                               index=['Test Statistics', 'p-value', 'No. of lags used',
-                                      'Number of observations used'])
-            for key, values in adft[4].items():
-                output['critical value (%s)' % key] = values
-
-        result = seasonal_decompose(df_close, model='multiplicative', freq=1)
-        summary_fig = plt.figure()
-        summary_fig = result.plot()
-        summary_fig.set_size_inches(16, 9)
-        st.subheader("Figure 3")
-        st.pyplot(summary_fig)
-
-        rcParams['figure.figsize'] = (10, 6)
-        df_log = np.log(df_close)
-        moving_avg = df_log.rolling(12).mean()
-        std_dev = df_log.rolling(12).std()
+    def test_stationarity(timeseries):
+        # Determing rolling statistics
+        rolmean = timeseries.rolling(12).mean()
+        rolstd = timeseries.rolling(12).std()
+        # Plot rolling statistics:
+        plt.plot(timeseries, color='blue', label='Original')
+        plt.plot(rolmean, color='red', label='Rolling Mean')
+        plt.plot(rolstd, color='black', label='Rolling Std')
         plt.legend(loc='best')
-        plt.title('Moving Average')
-        plt.plot(std_dev, color="black", label="Standard Deviation")
-        plt.plot(moving_avg, color="red", label="Mean")
-        plt.legend()
+        plt.title('Rolling Mean and Standard Deviation')
+        plt.show(block=False)
 
+        adft = adfuller(timeseries, autolag='AIC')
+        # output for dft will give us without defining what the values are.
+        # hence we manually write what values does it explains using a for loop
+        output = pd.Series(adft[0:4],
+                           index=['Test Statistics', 'p-value', 'No. of lags used',
+                                  'Number of observations used'])
+        for key, values in adft[4].items():
+            output['critical value (%s)' % key] = values
 
-    with part4:
-        st.subheader("Figure 4")
-        # split data into train and training set
-        train_data, test_data = df_log[3:int(len(df_log) * 0.9)], df_log[int(len(df_log) * 0.9):]
-        predict_fig = plt.figure(figsize=(10, 6))
-        plt.grid(True)
-        plt.xlabel('Dates')
-        plt.ylabel('Closing Prices')
-        plt.plot(df_log, 'green', label='Train data')
-        plt.plot(test_data, 'blue', label='Test data')
-        plt.legend()
+    result = seasonal_decompose(df_close, model='multiplicative', freq=1)
+    summary_fig = plt.figure()
+    summary_fig = result.plot()
+    summary_fig.set_size_inches(16, 9)
+    st.subheader("Figure 3")
+    st.pyplot(summary_fig)
 
-        st.pyplot(predict_fig)
+    rcParams['figure.figsize'] = (10, 6)
+    df_log = np.log(df_close)
+    moving_avg = df_log.rolling(12).mean()
+    std_dev = df_log.rolling(12).std()
+    plt.legend(loc='best')
+    plt.title('Moving Average')
+    plt.plot(std_dev, color="black", label="Standard Deviation")
+    plt.plot(moving_avg, color="red", label="Mean")
+    plt.legend()
 
-    part5, part6 = st.beta_columns(2)
-    with part5:
-        model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
-                                     test='adf',  # use adftest to find             optimal 'd'
-                                     max_p=3, max_q=3,  # maximum p and q
-                                     m=1,  # frequency of series
-                                     d=None,  # let model determine 'd'
-                                     seasonal=False,  # No Seasonality
-                                     start_P=0,
-                                     D=0,
-                                     trace=True,
-                                     error_action='ignore',
-                                     suppress_warnings=True,
-                                     stepwise=True)
+    st.subheader("Figure 4")
+    # split data into train and training set
+    train_data, test_data = df_log[3:int(len(df_log) * 0.9)], df_log[int(len(df_log) * 0.9):]
+    predict_fig = plt.figure(figsize=(10, 6))
+    plt.grid(True)
+    plt.xlabel('Dates')
+    plt.ylabel('Closing Prices')
+    plt.plot(df_log, 'green', label='Train data')
+    plt.plot(test_data, 'blue', label='Test data')
+    plt.legend()
 
-        fig_5 = model_autoARIMA.plot_diagnostics(figsize=(15, 8))
+    st.pyplot(predict_fig)
 
-        st.subheader("Figure 5")
-        st.write(fig_5)
-    with part6:
-        model = ARIMA(train_data, order=(3, 1, 2))
-        fitted = model.fit(disp=-1)
+    model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
+                                 test='adf',  # use adftest to find             optimal 'd'
+                                 max_p=3, max_q=3,  # maximum p and q
+                                 m=1,  # frequency of series
+                                 d=None,  # let model determine 'd'
+                                 seasonal=False,  # No Seasonality
+                                 start_P=0,
+                                 D=0,
+                                 trace=True,
+                                 error_action='ignore',
+                                 suppress_warnings=True,
+                                 stepwise=True)
 
-        # Forecast
-        fc, se, conf = fitted.forecast(implies_value, alpha=0.05)  # 95% confidence
-        fc_series = pd.Series(fc, index=test_data.index)
-        lower_series = pd.Series(conf[:, 0], index=test_data.index)
-        upper_series = pd.Series(conf[:, 1], index=test_data.index)
-        fig_6 = plt.figure(figsize=(12, 5), dpi=100)
-        plt.plot(train_data, label='training')
-        plt.plot(test_data, color='blue', label='Actual Stock Price')
-        plt.plot(fc_series, color='orange', label='Predicted Stock Price')
-        plt.fill_between(lower_series.index, lower_series, upper_series,
-                         color='k', alpha=.10)
-        plt.title('Altaba Inc. Stock Price Prediction')
-        plt.xlabel('Time')
-        plt.ylabel('Actual Stock Price')
-        plt.legend(loc='upper left', fontsize=8)
+    fig_5 = model_autoARIMA.plot_diagnostics(figsize=(15, 8))
 
-        # report performance
-        mse = mean_squared_error(test_data, fc)
+    st.subheader("Figure 5")
+    st.write(fig_5)
 
-        mae = mean_absolute_error(test_data, fc)
+    model = ARIMA(train_data, order=(3, 1, 2))
+    fitted = model.fit(disp=-1)
 
-        rmse = math.sqrt(mean_squared_error(test_data, fc))
+    # Forecast
+    fc, se, conf = fitted.forecast(implies_value, alpha=0.05)  # 95% confidence
+    fc_series = pd.Series(fc, index=test_data.index)
+    lower_series = pd.Series(conf[:, 0], index=test_data.index)
+    upper_series = pd.Series(conf[:, 1], index=test_data.index)
+    fig_6 = plt.figure(figsize=(12, 5), dpi=100)
+    plt.plot(train_data, label='training')
+    plt.plot(test_data, color='blue', label='Actual Stock Price')
+    plt.plot(fc_series, color='orange', label='Predicted Stock Price')
+    plt.fill_between(lower_series.index, lower_series, upper_series,
+                     color='k', alpha=.10)
+    plt.title('Altaba Inc. Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Actual Stock Price')
+    plt.legend(loc='upper left', fontsize=8)
 
-        mape = np.mean(np.abs(fc - test_data) / np.abs(test_data))
+    # report performance
+    mse = mean_squared_error(test_data, fc)
 
-        st.subheader("Figure 6")
-        st.pyplot(fig_6)
+    mae = mean_absolute_error(test_data, fc)
+
+    rmse = math.sqrt(mean_squared_error(test_data, fc))
+
+    mape = np.mean(np.abs(fc - test_data) / np.abs(test_data))
+
+    st.subheader("Figure 6")
+    st.pyplot(fig_6)
 
     Fmse = '{0:.3f}'.format(mse)
     st.write('MSE: '+Fmse)
